f76dcd1c255d2f090c3fb44ab05b1b992b1dd5ac
mosesfawole <mosesfawole2001@gmail.com>
2023-09-28 09:21:26 +0100

    initial navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome &nbsp;
+        <span className="whitespace-nowrap">
+          I'm <span className="font-bold">Moses</span>
+        </span>
+      </p>
+    </main>
+  );
 }
